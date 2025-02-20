b396408a8deddaab714c3590dd1b59173c41c513
Терминал1 <term1@classroom.local>
2025-02-20 10:27:02 +0300

    Comment

---
+++ src/test/prog/Calculator.java
@@ -0,0 +1,24 @@
+package test.prog;
+
+public class Calculator {
+    Calculator() {
+
+    }
+
+    protected double sum(double a, double b) {
+        return a + b;
+    }
+
+    protected double mul(double a, double b) {
+        return a * b;
+    }
+
+    protected double div(double a, double b) {
+        return a / b;
+    }
+
+    protected double sub(double a, double b) {
+        return a - b;
+    }
+
+}
---
+++ src/test/prog/Main.java
@@ -0,0 +1,26 @@
+package test.prog;
+
+import javax.print.attribute.SetOfIntegerSyntax;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+		Calculator calc = new Calculator();
+		Scanner scan = new Scanner(System.in);
+
+		try {
+			System.out.println("Input A: ");
+			double a = scan.nextDouble();
+			System.out.println("Input B: ");
+			double b = scan.nextDouble();
+
+			System.out.printf("\n%.2f + %.2f = %.2f", a, b, calc.sum(a,b));
+			System.out.printf("\n%.2f - %.2f = %.2f", a, b, calc.sub(a,b));
+			System.out.printf("\n%.2f * %.2f = %.2f", a, b, calc.mul(a,b));
+			System.out.printf("\n%.2f / %.2f = %.2f", a, b, calc.div(a,b));
+		} catch (Exception e) {
+			System.out.println("ERROR: " + e);
+		}
+    }
+}
