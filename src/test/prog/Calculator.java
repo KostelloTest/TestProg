648bde5fb56482f4e933646df8f83b5d3ea937a5
KostelloTest <kostya040103@gmail.com>
2025-02-27 10:18:38 +0300

    Update Calculator.java

--- src/test/prog/Calculator.java
+++ src/test/prog/Calculator.java
@@ -1,23 +1,53 @@
 package test.prog;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Calculator {
+
+    double a = 0;
+    double b = 0;
+    String operation;
+
     Calculator() {
 
     }
 
-    protected double sum(double a, double b) {
+    protected double getExp(String s) {
+        String [] lst = null;
+        lst = s.split(" ");
+
+        this.a = Double.parseDouble(lst[0]);
+        this.operation = lst[1];
+        this.b = Double.parseDouble(lst[2]);
+
+
+        switch (operation) {
+            case "+":
+                return sum();
+            case "-":
+                return sub();
+            case "/":
+                return div();
+            case "*":
+                return mul();
+        }
+        return Double.MIN_VALUE;
+    }
+
+    protected double sum() {
         return a + b;
     }
 
-    protected double mul(double a, double b) {
+    protected double mul() {
         return a * b;
     }
 
-    protected double div(double a, double b) {
+    protected double div() {
         return a / b;
     }
 
-    protected double sub(double a, double b) {
+    protected double sub() {
         return a - b;
     }
 
